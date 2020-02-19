78faaf5c1cc0f3c2d171174a71ba52de51f54c50
brunnom <brunomatheuslc@hotmail.com>
2020-02-19 09:58:44 -0300

    second commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
+import 'package:minhapp/widgets/input.widget.dart';
+import 'package:minhapp/widgets/logo.widget.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Alcool ou Gasolina',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  var _gasCtrl = new MoneyMaskedTextController();
+  var _alcCtrl = new MoneyMaskedTextController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: ListView(
+        children: <Widget>[
+          Logo(),
+          Input(
+            ctrl: _gasCtrl,
+            label: "Gasolina",
+          ),
+          Input(
+            ctrl: _alcCtrl,
+            label: "Álcool",
+          )
+        ],
+      ),
+    );
+  }
+}
